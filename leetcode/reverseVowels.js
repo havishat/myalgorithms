112378919416f905d118a4e3ab8e5fcfade6baf1
havishat <sundariha@gmail.com>
2018-05-09 21:09:06 -0700

    reverse Vowels

---
+++ leetcode/reverseVowels.js
@@ -0,0 +1,59 @@
+function reverseVowels(word){
+    var s = 0
+    var e = word.length; 
+    
+  //   while(s<e){
+  // //     for(var i=0;i<temp.length;i++){
+  //       if(helper(word[s]) && helper(word[e])){
+  //         temp = word[s]
+  //         word[s] = word[e]
+  //         word[e] =  temp
+  //         console.log(word[s])
+  //       }
+  //     s++
+  //     e--
+  //   }
+  
+  
+    while (s < e)
+      {
+        // console.log(helper(word[e]))
+        if (!helper(word[s]))
+        {
+            s++;
+            continue;
+        }
+        if (!helper(word[e]))
+        {
+            e--;
+            continue;
+        }
+   
+        // swapping
+        temp = word[s]
+        word[s] = word[e]
+        word[e] =  temp
+
+        s++;
+        e--;
+    }
+  
+    
+    
+    return word
+    
+    
+}
+  
+function helper(c){
+    // var vowels = ["a","e","i","o","u"];
+    return (c=='a' || c=='A' || c=='e' ||
+                c=='E' || c=='i' || c=='I' ||
+                c=='o' || c=='O' || c=='u' ||
+                c=='U');
+}
+  
+  
+  
+  
+console.log(reverseVowels(["w","h","i","t","e","b","o","a","r","d"]))
