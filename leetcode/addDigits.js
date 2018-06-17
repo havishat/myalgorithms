490699502903e63ab77296ff455ec17cfdda0703
havishat <sundariha@gmail.com>
2018-06-17 14:03:34 -0700

    258. Add Digits

--- leetcode/addDigits.js
+++ leetcode/addDigits.js
@@ -14,3 +14,16 @@ var addDigits = function(num) {
 };
 
 console.log(addDigits(3))
+
+
+var addDigits = function(num) {
+    if (num < 10) {
+        return num;
+    }
+    var sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num = Math.floor(num/10);
+    }
+    return addDigits(sum);
+};
