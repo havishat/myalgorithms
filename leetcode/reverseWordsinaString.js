d74f6fca8b15683c2809bba28d2857b7be98d148
havishat <sundariha@gmail.com>
2018-06-18 10:53:44 -0700

    151. Reverse Words in a String

--- leetcode/reverseWordsinaString.js
+++ leetcode/reverseWordsinaString.js
@@ -1,3 +1,17 @@
+// Given an input string, reverse the string word by word.
+
+// Example:  
+
+// Input: "the sky is blue",
+// Output: "blue is sky the".
+// Note:
+
+// A word is defined as a sequence of non-space characters.
+// Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+// You need to reduce multiple spaces between two words to a single space in the reversed string.
+// Follow up: For C programmers, try to solve it in-place in O(1) space.
+
+
 var reverseWords = function(str) {
    
     arr = str.trim().split(/\s+/)
@@ -17,4 +31,17 @@ var reverseWords = function(str) {
     }
     return str
       
-  };
+};
+
+console.log(reverseWords("the sky is blue"))
+
+// var reverseWords = function(str) {
+//     let words = str.split(' ');
+//     let newWords = [];
+//     for (let i = words.length - 1; i >= 0; --i) {
+//         if (words[i].length > 0) {
+//             newWords.push(words[i]);
+//         }
+//     }
+//     return newWords.join(' ');
+// };
