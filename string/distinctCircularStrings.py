6d9051a7fdeb80dcc514fa517816e5e85fd3f35a
havishat <sundariha@gmail.com>
2018-07-09 17:02:19 -0700

    geeks of geeks

--- string/distinctCircularStrings.py
+++ string/distinctCircularStrings.py
@@ -1,3 +1,18 @@
+# Print all distinct circular strings of length M in lexicographical order
+# Given a string and an integer M, print all distinct circular strings of length M in lexicographical order.
+
+# Examples:
+
+# Input: str = “baaaa”, M = 3
+# Output: aaa aab aba baa
+# All possible circular substrings of length 3 are “baa” “aaa” “aaa” “aab” “aba”
+# Out of the 6, 4 are distinct, and the lexicographical order is aaa aab aba baa
+
+# Input: str = “saurav”, M = 3
+# Output: aura avsa ravs saur urav vsau
+# All possible circular substrings of length 4 are saur aura urav ravs avsa vsau.
+# All the substrings are distinct, the lexicographical order is aura avsa ravs saur urav vsau.
+
 def distrinctCircleString(s, m):
   l = len(s)
   s2 = s + s
